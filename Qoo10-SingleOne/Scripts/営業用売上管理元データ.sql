af6903a257dc0f6c2614a2df365c17df8e31b98d
銘苅耕作 <@10.90.252.72>
2026-01-08 12:31:42 +0900

    v_dashbordではなくad_statからデータを取得

--- Qoo10-SingleOne/Scripts/営業用売上管理元データ.sql
+++ Qoo10-SingleOne/Scripts/営業用売上管理元データ.sql
@@ -1,50 +1,52 @@
 WITH account_table AS (
     SELECT 
-        ac.account_id,
-        ac.name AS account_name,
-        ac.account_group_id,
-        ac.account_category_id,
+        a.account_id,
+        a.name AS account_name,
+        a.account_group_id,
+        ag.company_name,
+        a.country_code,
+        a.account_category_id,
         ca.name AS category_name
     FROM 
-        account ac
-    LEFT JOIN 
-        account_category ca  ON ac.account_category_id = ca.account_category_id 
+        account a 
+        LEFT JOIN account_group ag ON a.account_group_id = ag.account_group_id 
+        LEFT JOIN account_category ca ON a.account_category_id = ca.account_category_id 
+),
+filtered_costs AS ( 
+    SELECT
+        t.account_id,
+        t.media,
+        t.collect_date,
+        SUM(t.cost) AS cost,
+        SUM(t.impression) AS report_imp,
+        SUM(t.click) AS report_click,
+        SUM(t.cv_conversion) AS report_cv,
+        SUM(t.cv_purchased) AS report_purchased,
+        SUM(t.cv_add_cart_conversion) AS report_cart_cv
+    FROM ad_stat t  
+    WHERE t.collect_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 7 DAY) 
+        AND t.collect_date < CURRENT_DATE()
+    GROUP BY t.account_id, t.collect_date, t.media
 )
 SELECT 
-	at.account_id,
-	at.account_name,
-	at.account_group_id,
-	at.category_name,
-	vd.adgroup_id,
-	vd.adgroup_name,
-	vd.collect_date,
-	SUM(vd.cost) AS cost,
-	SUM(vd.impression) AS imp,
-	SUM(vd.cost) / NULLIF(SUM(vd.impression)/1000, 0) AS cpm,
-	SUM(vd.click) AS cl,
-	SUM(vd.click) / NULLIF(SUM(vd.impression), 0) AS ctr,
-	SUM(vd.cost)/SUM(vd.click) AS cpc,
-	SUM(vd.cv_conversion) AS cv,
-	SUM(vd.cv_conversion) / NULLIF(SUM(vd.click), 0) AS cvr,
-	SUM(vd.cost) / NULLIF(SUM(vd.cv_conversion), 0) AS cpa,
-	SUM(vd.cv_purchased) AS sales,
-	SUM(vd.cv_purchased) / NULLIF(SUM(vd.cost), 0) AS ROAS,
-	SUM(vd.cv_add_cart_conversion) cart,
-	SUM(vd.cv_add_cart_conversion) / NULLIF(SUM(vd.click), 0) AS cart_cvr,
-	SUM(vd.cost) / NULLIF(SUM(vd.cv_add_cart_conversion), 0) AS cart_cpa
-FROM 
-	v_dashboard vd 
-	left join account_table at on vd.account_id = at.account_id 
-WHERE
-	vd.collect_date BETWEEN '${start_date}' AND '${end_date}'
-	AND vd.cost > 0
-	AND at.account_id IS NOT NULL
-	/*AND at.account_id = '${account_id}'*/
-GROUP BY 
     at.account_id,
-	at.account_name,
-	at.account_group_id,
-	at.category_name,
-	vd.adgroup_id,
-	vd.adgroup_name,
-	vd.collect_date
+    at.account_name,
+    at.account_group_id,
+    at.company_name,
+    at.country_code,
+    at.category_name,
+    fc.collect_date,
+    fc.media,
+    fc.cost,
+    fc.report_imp,
+    fc.report_click,
+    fc.report_cv,
+    fc.report_purchased,
+    fc.report_cart_cv
+FROM 
+    account_table at
+    LEFT JOIN filtered_costs fc ON at.account_id   = fc.account_id 
+WHERE
+    ( fc.cost IS NOT NULL AND fc.cost > 0 )
+    OR ( fc.report_cv IS NOT NULL AND fc.report_cv >= 1 )
+order by fc.collect_date ,at.account_id
